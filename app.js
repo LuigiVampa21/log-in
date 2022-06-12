8d56f8628016087279fa7181776afd9f28625096
TEISSIER Nathan <luigivampa12@protonmail.ch>
2022-06-12 14:52:01 +0200

    one last bug

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 'use strict';
 const btnRegister = document.querySelector('.btnRegister');
 const flexFormConnexion = document.querySelector('.flexFormConnexion');
+const flexFormRegister = document.querySelector('.flexFormRegister');
 const espaceConnexion = document.querySelector('.espaceConnexion')
 
 btnRegister.addEventListener('click', () => {
- espaceConnexion.classList.add('changeTO');   
-//  flexFormConnexion.style.display = 'none';
- btnRegister.textContent = 'login';
- btnRegister.style.transform = 'rotateY(180deg)'
+    if(btnRegister.textContent === 'Register'){
+        espaceConnexion.classList.add('changeTO');   
+        btnRegister.textContent = 'login';
+        espaceConnexion.classList.remove('Back');   
+        btnRegister.style.transform = 'rotateY(180deg)'
+    }else if(btnRegister.textContent === 'login'){
+        espaceConnexion.classList.remove('changeTO');
+        espaceConnexion.classList.add('Back');   
+        btnRegister.textContent = 'Register';
+        btnRegister.style.transform = 'rotateY(180deg) translateX(-370%)';
+    } 
+
 });
-if(btnRegister.textContent === 'login'){
-    // show flexFormRegister
-}
+// if(btnRegister.textContent === 'login'){
+//     // show flexFormRegister
+// }
+
+console.log(btnRegister.textContent === 'Register');
+console.log(btnRegister.textContent);
